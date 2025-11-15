0d9a8e4f87c51b6692062c41fc2dbae305fef69a
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-15 14:31:48 +0545

    Add: Routes Added

---
+++ controller/login.js
@@ -0,0 +1,6 @@
+const login = async(req,res)=>{
+    return 'Login';
+}
+
+
+module.exports = {login}
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const {login} = require('../controller/login')
+
+
+router.route('/login').get(login)
+
+
+module.exports = router
